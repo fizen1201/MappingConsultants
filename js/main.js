946077c44d03dc281e87fc060f81f155a46b2e50
visheshkoul@gmail.com <visheshkoul@gmail.com>
2022-04-17 22:58:22 +0530

    Updated Security in js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,39 @@
 
 !(function($) {
   "use strict";
-  // Security
+  // Security and removing ctrl+c etc functions
+  jQuery(document).ready(function($){
+    $(document).keydown(function(event) {
+    var pressedKey = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    if (event.ctrlKey && (pressedKey == "c" || pressedKey == "u")) {
+    alert('Sorry, This Functionality Has Been Disabled!');
+    //disable key press porcessing
+    return false;
+    }
+    });
+    });
 
+// f12 remove
+// document.onkeypress = function (event) {
+//   event = (event || window.event);
+//   if (event.keyCode == 123) {
+//   return false;
+//   }
+//   }
+//   document.onmousedown = function (event) {
+//   event = (event || window.event);
+//   if (event.keyCode == 123) {
+//   return false;
+//   }
+//   }
+//   document.onkeydown = function (event) {
+//   event = (event || window.event);
+//   if (event.keyCode == 123) {
+//   return false;
+//   }
+//   }
+// Removed Right Click
   document.addEventListener("contextmenu", function (e) {
     e.preventDefault();
   });
